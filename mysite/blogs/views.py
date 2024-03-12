bd512e021e2a738e8938196ef70aaf8c6dc633e4
kittipong0905 <kittipongtamzz@gmail.com>
2024-03-12 23:17:58 +0700

    create model category blog and setting url_img

--- mysite/blogs/views.py
+++ mysite/blogs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from category.models import Category
 
 # Create your views here.
 def Index(request):
     # return HttpResponse("<h1>Hello Django Framework</h1>")
-    return render(request,"frontend/index.html")
+    categories = Category.objects.all()
+    return render(request,"frontend/index.html",{'categories':categories})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blogs"
+    "blogs",
+    "category"
 ]
 
 MIDDLEWARE = [
@@ -80,6 +82,7 @@ DATABASES = {
         "NAME": 'myblogs',
         "USER": "root",
         "PASSWORD":""
+
     }
 }
 
@@ -118,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
